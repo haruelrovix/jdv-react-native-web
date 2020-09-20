c9ca2367c60045013940a9bf8fa8c0064b107c9d
haruelrovix <havit@adakerja.com>
2020-09-21 00:24:47 +0700

    chore: Fix RNVI issue for the Web version

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -349,12 +349,13 @@ module.exports = function(webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.ttf$/],
               loader: require.resolve('url-loader'),
               options: {
                 limit: imageInlineSizeLimit,
                 name: 'static/media/[name].[hash:8].[ext]',
               },
+              include: path.join(paths.appNodeModules, 'react-native-vector-icons'),
             },
             // Process application JS with Babel.
             // The preset includes JSX, Flow, TypeScript, and some ESnext features.
